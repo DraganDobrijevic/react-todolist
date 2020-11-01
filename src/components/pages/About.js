93542bc645fe1dba862995e83b26dc9325d54339
dragandobrijevic <dragan16m@gmail.com>
2020-11-02 00:00:19 +0100

    Christmas

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,70 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 export default function About() {
   const containerRef = useRef(null);
+  const [name, setName] = useState('007');
+  const inputNameRef = useRef(null);
+
+  useEffect(() => {
+    let name = localStorage.getItem('name')
+    if ( name !== '007' && name !== 'undefined') {
+      // localStorage.setItem('name', '007');
+      setName(name);
+      console.log('setNameEffect  '+ name );
+    }
+  }, [name])
+
+  const addNameHandler = e => {
+    e.preventDefault();
+    const newName = inputNameRef.current.value;
+    setName(newName);
+    localStorage.setItem('name', newName);
+    console.log("setNameHandler" + newName);
+    inputNameRef.current.value = "";
+  }
+
+  // Load current value of name
+  // let displayName = document.querySelector('#display-name');
+  // displayName.innerHTML = localStorage.getItem('name');
+
+  // Start animation - hello
+  // displayName.parentElement.style.animationPlayState = 'running';
+
+  // if (localStorage.getItem('name') !== '007') {
+
+  //     // Delete form
+  //     document.querySelector('.btn').parentElement.remove();
+
+  //     // Start animation - channelsA
+  //     document.querySelector('#channels').style.animationPlayState = 'running';
+  // } 
+  // else {
+
+  //     // On click get name
+  //     document.querySelector('#form').onsubmit = () => {
+      
+  //         let name = document.querySelector('#name').value;
+  //         console.log(name)
+
+  //         // Set name
+  //         let displayName = document.querySelector('#display-name');
+  //         displayName.innerHTML = name;
+  //         localStorage.setItem('name', name);
+
+  //         // Start animation - hello
+  //         displayName.parentElement.style.animationPlayState = 'running';
+
+  //         // Start animation - channelsA
+  //         document.querySelector('#channels').style.animationPlayState = 'running';
+
+  //         // Delete form
+  //         document.querySelector('.btn').parentElement.remove();
+
+  //         return false;
+
+  //     }
+  // }
+
 
   return (
     <div className="boxAbout">
@@ -14,8 +77,17 @@ export default function About() {
             containerRef.current.classList.remove('hover-left')
           }}
       >
-          <h1 className="h1">The Designer</h1>
-          <a href="https://www.dragandobrijevic.com" target="_blank" rel="noopener noreferrer" className="button">Read More</a>
+          <form id="form" onSubmit={addNameHandler}>
+            <input ref={inputNameRef} id="name" autoComplete="off" autoFocus placeholder="Name" type="text"/>
+            <button className="btn" type="submit">Add</button>
+          </form>
+    
+          <div id="hello-name">
+            Hello <span id="display-name">{name}</span>!
+          </div>
+
+          {/* <h1 className="h1">The Designer</h1>
+          <a href="https://www.dragandobrijevic.com" target="_blank" rel="noopener noreferrer" className="button">Read More</a> */}
       </div>
       <div className="split right"
            onMouseEnter={() => {
@@ -25,7 +97,8 @@ export default function About() {
             containerRef.current.classList.remove('hover-right')
           }}
       >
-        <h1 className="h1">The Programmer</h1>
+        <h1 className="h1">React Todo List App</h1>
+        <h2 className="h2">Created by Dragan Dobrijević</h2>
         <a href="https://www.dragandobrijevic.com" target="_blank" rel="noopener noreferrer" className="button">Read More</a>
       </div>
     </div>
